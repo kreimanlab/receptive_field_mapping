8d473b147d651ca3938d428fbe6454c21bbe3f89
Meash <m4rtinsch@gmail.com>
2016-07-29 19:03:07 -0400

    add parseEEG

---
+++ analyze/parseEEG.m
@@ -0,0 +1,25 @@
+function data = parseEEG(filenameIn)
+filenameOut = [filenameIn, '.tmp'];
+fin = fopen(filenameIn, 'r');
+fout = fopen(filenameOut, 'wt');
+lineNumber = 0;
+while true
+    line = fgetl(fin);
+    if ~ ischar(line)
+        break;
+    end
+    lineNumber = lineNumber + 1;
+    if lineNumber <= 13 || lineNumber == 15
+        continue;
+    elseif lineNumber == 14
+        line = line(3:end);
+        line = strrep(line, 'Date.Time', 'Date Time');
+    end
+    line = regexprep(line, '[\t ]+', '\t');
+    fprintf(fout, '%s\n', line);
+end
+fclose(fin);
+fclose(fout);
+data = dataset('File', filenameOut);
+delete(filenameOut);
+end
